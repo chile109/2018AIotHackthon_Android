58f4077ee0c70ae147ce7baf7ce79d4d552bab4f
Kevin Chang <kevin@megalive.com.tw>
2018-10-04 15:32:23 +0800

    [Update] 不出現dialog進行語音判斷 新增狀況判斷

--- SpeachRecognizationTest/app/src/main/java/app/kevin/com/speachrecognizationtest/MainActivity.java
+++ SpeachRecognizationTest/app/src/main/java/app/kevin/com/speachrecognizationtest/MainActivity.java
@@ -6,95 +6,175 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends Activity {
-    static String TAG = "main";
-    private SpeechRecognizer recognizer;
+public class MainActivity extends Activity implements RecognitionListener {
+    private String Tag = "VoiceRecognitionActivity";
+    private TextView returnedText;
+    private ProgressBar progressBar;
+    private ToggleButton toggleButton;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById( R.id.button);
+        returnedText = (TextView) findViewById(R.id.textView1);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton1);
 
         // 判斷裝置是否有支援語音辨識功能的 App, 若沒有則失效之
         PackageManager pm = getPackageManager();
         //---------查詢有無裝Google Voice Search Engine---------
-        List<ResolveInfo> activities=pm.queryIntentActivities(
-                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),0);
+        List<ResolveInfo> activities = pm.queryIntentActivities(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
         //-------------如果有找到-------------
-        if(activities.size()!=0){
-            try{
+        if (activities.size() != 0) {
+            try {
 
-        //------------語音辨識Intent-----------
-                Intent intent =new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                progressBar.setVisibility(View.INVISIBLE);
+                speech = SpeechRecognizer.createSpeechRecognizer(this);
+                speech.setRecognitionListener(this);
+                recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en");
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
 
-//                intent.putExtra
-//                        (
-//                                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-//                                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-//                        );
+                toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            progressBar.setVisibility(View.VISIBLE);
+                            progressBar.setIndeterminate(true);
+                            speech.startListening(recognizerIntent);
+                        } else {
+                            progressBar.setIndeterminate(false);
+                            progressBar.setVisibility(View.INVISIBLE);
+                            speech.stopListening();
+                        }
+                    }
+                });
 
-                intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, Locale.TRADITIONAL_CHINESE.toString() );
-                intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);//設定只出現辨識結果第一筆
-
-                // ----------開啟語音辨識Intent-----------
-                startActivityForResult( intent, 0 );
-
-            }catch(Exception e){//catch error message
+            } catch (Exception e) {//catch error message
                 new AlertDialog.Builder(MainActivity.this)
                         .setTitle("Error")
-                        .setMessage(e.getMessage()).show();;
+                        .setMessage(e.getMessage()).show();
+                ;
             }
-        }else{//----------如果找不到-------------
+        } else {//----------如果找不到-------------
             Toast.makeText(MainActivity.this
                     , "找不到語音辨識 App !!"
                     , Toast.LENGTH_LONG
             ).show();
 
 //且導向Market Google語音下載網頁 讓使用者下載
-            String url="https://market.android.com/details?id=com.google.android.voicesearch";
+            String url = "https://market.android.com/details?id=com.google.android.voicesearch";
             Intent ie = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 //啟動Intent
             startActivity(ie);
         }
     }
 
+    //region recognizer
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if( requestCode == 0 && resultCode == RESULT_OK )
-        {
-            String resultsString = "";
+    public void onReadyForSpeech(Bundle params) {
 
-            // 取得 STT 語音辨識的結果段落
-            ArrayList results = data.getStringArrayListExtra( RecognizerIntent.EXTRA_RESULTS );
+    }
 
-            // 語音辨識的每個段落
-            for( int i = 0; i < results.size(); i++ )
-            {
-                // 一個段落可拆解為多個字組
-                String[] resultWords = results.get(i).toString().split(" ");
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(Tag, "onBeginningOfSpeech");
+        progressBar.setIndeterminate(false);
+        progressBar.setMax(10);
+    }
 
-                for( int j = 0; j < resultWords.length; j++ )
-                {
-                    resultsString += resultWords[j] + ":";
-                }
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+        progressBar.setProgress((int) rmsdB);
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(Tag, "onEndOfSpeech");
+        progressBar.setIndeterminate(true);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onError(int error) {
+        toggleButton.setChecked(false);
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(Tag, "onResults");
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        String saying = "";
+        for (String result : matches) {
+
+            switch (result) {
+                case "hello":
+                    saying = "很高興認識你";
+                    break;
+                case "Kevin":
+                    saying = "美佳小文青";
+                    break;
+                case "bye":
+                    saying = "再見";
+                    break;
             }
 
-            // 顯示結果
-            Toast.makeText( this, resultsString, Toast.LENGTH_LONG ).show();
+            text += result + "\n";
         }
 
-        super.onActivityResult( requestCode, resultCode, data );
+        if (saying.isEmpty()) {
+            saying = "無法辨識";
+        }
+
+        Toast.makeText(MainActivity.this
+                , saying
+                , Toast.LENGTH_LONG
+        ).show();
+
+        returnedText.setText(text);
     }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
+    }
+    //endregion
 }
+
