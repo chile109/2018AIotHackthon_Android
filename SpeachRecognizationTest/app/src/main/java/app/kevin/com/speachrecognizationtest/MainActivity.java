cd94e38cfe3f76265453642df943b0f4faf01b5a
Kevin Chang <kevin@megalive.com.tw>
2018-10-19 23:29:35 +0800

    [Update] text to speech

--- SpeachRecognizationTest/app/src/main/java/app/kevin/com/speachrecognizationtest/MainActivity.java
+++ SpeachRecognizationTest/app/src/main/java/app/kevin/com/speachrecognizationtest/MainActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,18 +19,28 @@ import java.util.Locale;
 
 public class MainActivity extends Activity {
     static String TAG = "main";
-    private SpeechRecognizer recognizer;
+    private TextToSpeech tts;
     List<ResolveInfo> activities;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById( R.id.button);
+        Button button2 = (Button)findViewById( R.id.button2);
+        createLanguageTTS();
 
-        // 判斷裝置是否有支援語音辨識功能的 App, 若沒有則失效之
+        button2.setOnClickListener( new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0)
+            {
+                //【英文】發音
+                tts.speak( "good job", TextToSpeech.QUEUE_FLUSH, null );
+            }
+        });
+
+        Button button = (Button)findViewById( R.id.button);
         PackageManager pm = getPackageManager();
-        //---------查詢有無裝Google Voice Search Engine---------
+
         activities = pm.queryIntentActivities(
                 new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),0);
 
@@ -38,19 +49,11 @@ public class MainActivity extends Activity {
             @Override
 
             public void onClick(View v) {
-                //-------------如果有找到-------------
                 if(activities.size()!=0){
                     try{
 
                         //------------語音辨識Intent-----------
                         Intent intent =new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-//                intent.putExtra
-//                        (
-//                                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-//                                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-//                        );
-
                         intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, Locale.TRADITIONAL_CHINESE.toString() );
                         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);//設定只出現辨識結果第一筆
 
@@ -62,16 +65,14 @@ public class MainActivity extends Activity {
                                 .setTitle("Error")
                                 .setMessage(e.getMessage()).show();;
                     }
-                }else {//----------如果找不到-------------
+                }else {
                     Toast.makeText(MainActivity.this
                             , "找不到語音辨識 App !!"
                             , Toast.LENGTH_LONG
                     ).show();
 
-//且導向Market Google語音下載網頁 讓使用者下載
                     String url = "https://market.android.com/details?id=com.google.android.voicesearch";
                     Intent ie = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//啟動Intent
                     startActivity(ie);
                 }
 
@@ -107,5 +108,30 @@ public class MainActivity extends Activity {
         }
 
         super.onActivityResult( requestCode, resultCode, data );
+    }
+
+    private void createLanguageTTS()
+    {
+        if( tts == null )
+        {
+            tts = new TextToSpeech(this, new TextToSpeech.OnInitListener(){
+                @Override
+                public void onInit(int arg0)
+                {
+                    // TTS 初始化成功
+                    if( arg0 == TextToSpeech.SUCCESS )
+                    {
+                        // 指定的語系: 英文(美國)
+                        Locale l = Locale.US;  // 不要用 Locale.ENGLISH, 會預設用英文(印度)
+
+                        // 目前指定的【語系+國家】TTS, 已下載離線語音檔, 可以離線發音
+                        if( tts.isLanguageAvailable( l ) == TextToSpeech.LANG_COUNTRY_AVAILABLE )
+                        {
+                            tts.setLanguage( l );
+                        }
+                    }
+                }}
+            );
+        }
     }
 }
